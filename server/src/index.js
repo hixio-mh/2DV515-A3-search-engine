80c6d7d54ad9ff8ba836b947df6a05f9eb9aa3d8
antonstrand <jag@antonstrand.se>
2019-12-05 16:07:25 +0100

    Adding time logging

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,10 @@ const { initDatabase, allPages } = require('./repositories/pages')
 const query = require('./models/query')
 const L = require('list')
 
-initDatabase(console.error, _ =>
+console.time('start')
+initDatabase(console.error, _ => {
+  console.timeEnd('start')
+  console.time('query')
   console.log(L.toArray(query(allPages())('super mario')))
-)
+  console.timeEnd('query')
+})
