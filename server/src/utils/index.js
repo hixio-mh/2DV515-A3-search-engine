9c4c633225463a0311466e2b033cb108ae435424
Anton Strand <jag@antonstrand.se>
2019-12-09 21:31:52 +0100

    No longer removing () [] since it makes the results not match test cases

--- server/src/utils/index.js
+++ server/src/utils/index.js
@@ -3,9 +3,9 @@
 const List = require('list/curried')
 const isEmpty = require('crocks/predicates/isEmpty')
 const not = require('crocks/logic/not')
+const filter = require('ramda/src/filter')
 const pipe = require('ramda/src/pipe')
 const reduce = require('ramda/src/reduce')
-const replace = require('ramda/src/replace')
 const split = require('ramda/src/split')
 
 /** minimum :: Foldable f => f Number -> Number */
@@ -16,15 +16,15 @@ const maximum = reduce(Math.max, -Infinity)
 
 /** words :: String -> List String */
 const words = pipe(
-  replace(/[()]/g, ''),
-  split(' '),
+  split(/\s+/g),
   List.from,
-  List.filter(not(isEmpty))
+  List.reduce((ws, w) => !isEmpty(w) ? List.append(w.toLowerCase(), ws) : ws, List.empty())
 )
 
 /** lines :: String -> Set String */
 const lines = pipe(
   split('\n'),
+  filter(not(isEmpty)),
   xs => new Set(xs)
 )
 
