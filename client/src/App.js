b717f526d0c5ced059316d6b24400cd2f9914e7b
antonstrand <jag@antonstrand.se>
2019-12-10 17:01:42 +0100

    fixing layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,72 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function App () {
   return (
     <div className='wrapper'>
       <header>
         <h1>A3 - Search Engine</h1>
       </header>
       <main>
-        <input type='text' placeholder='Search...'></input>
-        <button type='submit'>Search</button>
+        <form>
+          <input type='text' placeholder='Search...' />
+          <button type='submit'>Search</button>
+        </form>
+        <table>
+          <tbody>
+            <tr>
+              <th className='alignLeft'>Page</th>
+              <th>Score</th>
+              <th>Content</th>
+              <th>Location</th>
+              <th>Page rank</th>
+            </tr>
+            <tr>
+              <td className='alignLeft'>Java programming as a concept</td>
+              <td>1</td>
+              <td>0.8</td>
+              <td>0.7</td>
+              <td>0.2</td>
+            </tr>
+            <tr>
+              <td className='alignLeft'>Java</td>
+              <td>1</td>
+              <td>0.8</td>
+              <td>0.7</td>
+              <td>0.2</td>
+            </tr>
+            <tr>
+              <td className='alignLeft'>Java</td>
+              <td>1</td>
+              <td>0.8</td>
+              <td>0.7</td>
+              <td>0.2</td>
+            </tr>
+            <tr>
+              <td className='alignLeft'>Java</td>
+              <td>1</td>
+              <td>0.8</td>
+              <td>0.7</td>
+              <td>0.2</td>
+            </tr>
+            <tr>
+              <td className='alignLeft'>Java</td>
+              <td>1</td>
+              <td>0.8</td>
+              <td>0.7</td>
+              <td>0.2</td>
+            </tr>
+          </tbody>
+        </table>
+        <p>Results in XX sec</p>
       </main>
-    <p style={{ margin: '1em 0 2em' }}>Created by <a href='https://github.com/antonstrand'>Anton Strand</a></p>
-  </div>
-  );
+      <footer>
+        <p>
+          Created by <a href='https://github.com/antonstrand'>Anton Strand</a>
+        </p>
+      </footer>
+    </div>
+  )
 }
 
-export default App;
+export default App
